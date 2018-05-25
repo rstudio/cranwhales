9bf45bae20f71ecdc03a9d5d0d4ed1fd0989f695
Joe Cheng <joe@rstudio.com>
2018-05-25 16:28:40 -0700

    Change default date

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ ui <- dashboardPage(
     title = "CRAN whales"
   ),
   dashboardSidebar(
-    dateInput("date", "Date", value = "2018-05-22"),
+    dateInput("date", "Date", value = Sys.Date() - 2),
     numericInput("count", "Show top N downloaders:", 6)
   ),
   dashboardBody(
